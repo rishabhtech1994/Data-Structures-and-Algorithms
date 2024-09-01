0d917dbb326b13072b3414f38373dfb22067d70a
rishabhtech1994 <rishi.gupta8819@gmail.com>
2024-09-01 15:28:04 +0530

    Queue with array

--- src/queue/QueueImpl.java
+++ src/queue/QueueImpl.java
@@ -1,7 +1,58 @@
 package queue;
 
 public class QueueImpl {
+    int count;
+    int front;
+    int rear;
+    int maxSize = Integer.MAX_VALUE;
+    int [] arr ;
+    // Constructor with Max Size of the array
+    public QueueImpl(int maxSize){
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+        front = 0;
+        rear =0;
+        count =0;
+    }
+
+    // TC == O(1)
+    public void push(int x){
+        if(count == maxSize) return;
+        arr[rear % maxSize] = x;
+        rear++;
+        count ++;
+    }
+
+    // TC == O(1)
+    public int pop(){
+        if(count == 0) return -1;
+        int x =  arr[front % maxSize] ;
+        front ++;
+        count --;
+        return x;
+    }
+
+    // TC O(1)
+    public int top(){
+        if(count == 0) return -1;
+        return arr[front % maxSize];
+    }
+
+    // TC == O(1)
+    public int size(){
+        return count;
+    }
+
     public static void main(String[] args) {
-        
+        QueueImpl q = new QueueImpl(6);
+        q.push(4);
+        q.push(14);
+        q.push(24);
+        q.push(34);
+        System.out.println("The peek of the queue before deleting any element " + q.top());
+        System.out.println("The size of the queue before deletion " + q.size());
+        System.out.println("The first element to be deleted " + q.pop());
+        System.out.println("The peek of the queue after deleting an element " + q.top());
+        System.out.println("The size of the queue after deleting an element " + q.size());
     }
 }
